1ce2a3ff1dd25fb6b76de86e31434e489acd5585
zhangpei529 <zhangpei529@vip.qq.com>
2019-04-15 15:34:31 +0800

    重构代码

--- CollectData/Untils/Gateio.py
+++ CollectData/Untils/Gateio.py
@@ -19,27 +19,27 @@ class GateWs:
         self.__api_key = api_key
         self.__secret_key = secret_key
 
-    def gate_get(self, id, method, params):
+    def gate_get(self, method, params):
         if params is None:
             params = []
         ws = create_connection(self.__url)
-        data = {'id': id, 'method': method, 'params': params}
+        data = {'id': self.generate_rand_number(), 'method': method, 'params': params}
         js = json.dumps(data)
         ws.send(js)
         return ws.recv()
 
-    def gate_request(self, id, method, params):
+    def gate_request(self, method, params):
         ws = create_connection(self.__url)
         nonce = int(time.time() * 1000)
         signature = get_sign(self.__secret_key, str(nonce))
-        data = {'id': id, 'method': 'server.sign', 'params': [self.__api_key, signature, nonce]}
+        data = {'id': self.generate_rand_number(), 'method': 'server.sign', 'params': [self.__api_key, signature, nonce]}
         js = json.dumps(data)
         ws.send(js)
         if method == "server.sign":
             return ws.recv()
         else:
             ws.recv()
-            data = {'id': id, 'method': method, 'params': params}
+            data = {'id': self.generate_rand_number(), 'method': method, 'params': params}
             js = json.dumps(data)
             ws.send(js)
             return ws.recv()
