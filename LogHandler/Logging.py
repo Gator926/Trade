73c0da45f5309e9296a8bd33a5d2fb54f9462486
gator <noshow@gmail.com>
2019-07-09 16:22:42 +0800

    封装了日志类

---
+++ LogHandler/Logging.py
@@ -0,0 +1,60 @@
+import logging
+import logging.handlers
+import os
+import time
+
+
+class LogHandler(object):
+    def __init__(self):
+        self.logger = logging.getLogger("")
+        # 设置输出的等级
+        LEVELS = {'NOSET': logging.NOTSET,
+                  'DEBUG': logging.DEBUG,
+                  'INFO': logging.INFO,
+                  'WARNING': logging.WARNING,
+                  'ERROR': logging.ERROR,
+                  'CRITICAL': logging.CRITICAL}
+        # 创建文件目录
+        logs_dir = "logs"
+        if os.path.exists(logs_dir) and os.path.isdir(logs_dir):
+            pass
+        else:
+            os.mkdir(logs_dir)
+        # 修改log保存位置
+        timestamp = time.strftime("%Y-%m-%d", time.localtime())
+        logfilename = '%s.txt' % timestamp
+        logfilepath = os.path.join(logs_dir, logfilename)
+        rotatingFileHandler = logging.handlers.RotatingFileHandler(filename=logfilepath,
+                                                                   maxBytes=1024 * 1024 * 50,
+                                                                   backupCount=15)
+        # 设置输出格式
+        formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter('%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s')
+        rotatingFileHandler.setFormatter(formatter)
+        # 控制台句柄
+        console = logging.StreamHandler()
+        console.setLevel(logging.NOTSET)
+        console.setFormatter(formatter)
+        # 添加内容到日志句柄中
+        self.logger.addHandler(rotatingFileHandler)
+        self.logger.addHandler(console)
+        self.logger.setLevel(logging.NOTSET)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+
+if __name__ == '__main__':
+    log_handler = LogHandler()
+    log_handler.info("test")
+    log_handler.warning("waringi")
+    log_handler.debug("warung")
