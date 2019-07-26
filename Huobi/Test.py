bc9905a36b17e4997ac6ce9acc315cd466ccc589
gator <noshow@gmail.com>
2019-07-26 15:54:34 +0800

    MarketHandler模块单元测试代码

--- Huobi/Test.py
+++ Huobi/Test.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 from Huobi.KeepBalanceStrategy import KeepBalanceStrategy
+from Huobi.MarketHandler import MarketHandler
+from MongdbHandler.MongodbHandler import MongodbHandler
+import copy
 
 
 class TestKeepBalanceStrategy(TestCase):
@@ -31,3 +34,46 @@ class TestKeepBalanceStrategy(TestCase):
     def test_get_price(self):
         price = self.strategy.get_price('btcusdt')
         self.assertGreaterEqual(price, 0)
+
+
+class TestMarketHandler(TestCase):
+    def setUp(self):
+        self.symbol_info = {'symbol': 'bhtusdt', 'quote_currency': 'usdt', 'base_currency': 'bht',
+                            'amount_precision': 2, 'price_precision': 6, 'symbol_partition': 'innovation'}
+        self.market_handler = MarketHandler()
+        self.mongodb_handler = MongodbHandler()
+
+    def test_check_currency_precision(self):
+        self.market_handler.check_currency_precision()
+
+    def test_get_currency_precision(self):
+        precision = self.market_handler.get_currency_precision()
+        self.assertIsInstance(precision, list)
+        self.assertIsInstance(precision[0], dict)
+
+    def test_check_symbol_info_with_empty_data(self):
+        self.mongodb_handler.precision_collection.delete_many({})
+
+        self.market_handler.check_symbol_info(self.symbol_info)
+        number = self.mongodb_handler.precision_collection.count_documents(
+            {"symbol": self.symbol_info['symbol']})
+        self.assertEqual(number, 1)
+
+    def test_check_symbol_info_with_changed_data(self):
+        self.mongodb_handler.precision_collection.delete_many({})
+
+        changed_symbol_info = copy.deepcopy(self.symbol_info)
+        changed_symbol_info['amount_precision'] = 3
+        self.market_handler.check_symbol_info(changed_symbol_info)
+        result = self.mongodb_handler.precision_collection.find_one(
+            {"symbol": self.symbol_info['symbol']})
+        self.assertEqual(result['amount_precision'], 3)
+
+    def test_change_dict_to_symbol(self):
+        symbol = self.market_handler.change_dict_to_symbol(self.symbol_info)
+        self.assertEqual(symbol.symbol, self.symbol_info['symbol'])
+        self.assertEqual(symbol.quote_currency, self.symbol_info['quote_currency'])
+        self.assertEqual(symbol.base_currency, self.symbol_info['base_currency'])
+        self.assertEqual(symbol.amount_precision, self.symbol_info['amount_precision'])
+        self.assertEqual(symbol.price_precision, self.symbol_info['price_precision'])
+        self.assertEqual(symbol.symbol_partition, self.symbol_info['symbol_partition'])
