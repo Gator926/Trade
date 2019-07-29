3db2b16995b7e126262afd4d8e07ba3c63d84972
gator <noshow@gmail.com>
2019-07-29 12:36:47 +0800

    邮件提示模块

---
+++ LogHandler/MailHandler.py
@@ -0,0 +1,32 @@
+import smtplib
+from LogHandler import LogHandler
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+class MailHandler(LogHandler):
+    def __init__(self):
+        LogHandler.__init__(self)
+        self.mail_host = self.get_config_value("mail", "host")
+        self.mail_user = self.get_config_value("mail", "user")
+        self.mail_password = self.get_config_value("mail", "password")
+        self.mail_port = self.get_config_value("mail", "port")
+        self.sender = self.get_config_value("mail", "sender")
+        self.receivers = self.get_config_value("mail", "receivers")
+
+    def send_mail(self, subject, content):
+        message = MIMEText(content, 'plain', 'utf-8')
+        message['Subject'] = Header(subject, 'utf-8')
+        try:
+            smtp_obj = smtplib.SMTP_SSL(self.mail_host, self.mail_port)
+            smtp_obj.login(self.mail_user, self.mail_password)
+            smtp_obj.sendmail(self.sender, self.receivers, message.as_string())
+            smtp_obj.quit()
+            self.logger.error(f"邮件发送成功: {message}")
+        except smtplib.SMTPException as e:
+            self.logger.error(e)
+
+
+if __name__ == '__main__':
+    mail_handler = MailHandler()
+    mail_handler.send_mail("subject", "content")
