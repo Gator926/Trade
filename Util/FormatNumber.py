cf746c957da3ead51b74cf3a71dc31a30db7e2ce
zhangpei529 <zhangpei529@vip.qq.com>
2019-08-06 16:48:58 +0800

    解决了'6.0'无法直接转为为int类型的debug

--- Util/FormatNumber.py
+++ Util/FormatNumber.py
@@ -2,6 +2,14 @@ from decimal import *
 
 
 def retain_decimals(number: str, precision: str) -> str:
+    """
+    保留小数位数代码
+    :param number:    数字
+    :param precision: 保留小数点后几位
+    :return:
+
+    int(float(precision)) 是为了解决'6.0'无法直接转为为int类型, 是否为产生其他的问题, 有待观察
+    """
     number = str(number)
     if "e" in number:
         value = number[:number.index("e")]
@@ -12,5 +20,5 @@ def retain_decimals(number: str, precision: str) -> str:
             number = Decimal(value[:number.index(".") + int(precision) + 1]) * (10 ** Decimal(base))
         return retain_decimals(str(number), precision)
     else:
-        number = number[:number.index(".") + int(precision) + 1]
+        number = number[:number.index(".") + int(float(precision)) + 1]
     return number
