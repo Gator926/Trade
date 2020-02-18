597cbeacc2774e4325560bf8b1d2d04c2606bb94
彼岸529 <zhangpei529@vip.qq.com>
2020-02-18 10:42:02 +0800

    删除了工作部分无用代码

--- CollectData/collect_data/Main.py
+++
@@ -1,5 +0,0 @@
-class Main:
-    def __init__(self):
-        self.name = "jack"
-
-
--- CollectData/collect_data/Settings.py
+++
@@ -1,7 +0,0 @@
-Settings = {
-    "Gate": {
-        "url": "wss://ws.gate.io/v3/",
-        "secret_key": "D26D21EA-D73A-4442-996C-C756206112F8",
-        "message": "34e852c9bbccf04333bb7fc94eb24b066a929aff7ebbc48a13428458511c8387"
-    }
-}
